8d5ec8c33bad6c9447d888aa8d19cbafef76b698
Salma Adzra Fathina <salmafthn@student.ub.ac.id>
2023-11-26 14:21:42 +0700

    captcha

--- public/js/main.js
+++ public/js/main.js
@@ -42,3 +42,25 @@ window.onscroll = function () {
 
   prevScrollpos = currentScrollPos;
 };
+
+
+// CAPTCHA
+var captcha = document.getElementById("fake-captcha");
+
+var passOrFail = function () {
+  var pass = Math.round(Math.random());
+  return pass ? "pass" : "fail";
+};
+
+captcha.onclick = function () {
+  if (captcha.className.includes("loading")) return;
+
+  captcha.className = "";
+
+  captcha.className += "loading";
+
+  setTimeout(function () {
+    captcha.className = captcha.className.replace("loading", "");
+    captcha.className += passOrFail();
+  }, Math.floor(Math.random() * 3000 + 1000));
+};
